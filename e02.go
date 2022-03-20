0c44f0bbf5964db00cfa1a3325d14d3f462f89df
Tom Nguyen <104503-tunggnu@users.noreply.gitlab.com>
2022-03-20 23:42:50 +0700

    Example 02

---
+++ e02.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func e02() {
+	fmt.Println("Welcome to the playground!")
+	fmt.Println("The time is", time.Now())
+}
--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 package main
 
 func main() {
-	e01()
+	e02()
 }
