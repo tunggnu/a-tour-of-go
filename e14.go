543f6dafa632ed2ca661cb61ae0639b6740e0662
Tom Nguyen <104503-tunggnu@users.noreply.gitlab.com>
2022-03-21 11:54:52 +0700

    Example 14

---
+++ e14.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func e14() {
+	var i int
+	var f float64
+	var b bool
+	var s string
+	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+}
--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 package main
 
 func main() {
-	e13()
+	e14()
 }
