8406fd012327b660afff80146f422b3fc6ddf4f7
Tom Nguyen <104503-tunggnu@users.noreply.gitlab.com>
2022-03-21 10:08:19 +0700

    Example 10

---
+++ e10.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+var c, python, java bool
+
+func e10() {
+	var i int
+	fmt.Println(i, c, python, java)
+}
--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 package main
 
 func main() {
-	e09()
+	e10()
 }
