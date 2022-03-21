e7f2cbf3f2e2ada2e9373337d2a5c2f9e04cfdf9
Tom Nguyen <104503-tunggnu@users.noreply.gitlab.com>
2022-03-21 09:40:40 +0700

    Example 06

---
+++ e06.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func add(x int, y int) int {
+	return x + y
+}
+
+func e06() {
+	fmt.Println(add(42, 13))
+}
--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 package main
 
 func main() {
-	e05()
+	e06()
 }
