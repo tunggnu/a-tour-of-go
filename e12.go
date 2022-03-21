30f2fd6cd7b4611acd4f5f8da2497639ee4f8e5f
Tom Nguyen <104503-tunggnu@users.noreply.gitlab.com>
2022-03-21 10:15:31 +0700

    Example 12

---
+++ e12.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func e12() {
+	var i, j int = 1, 2
+	k := 3
+	c, python, java := true, false, "no!"
+
+	fmt.Println(i, j, k, c, python, java)
+}
--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 package main
 
 func main() {
-	e11()
+	e12()
 }
