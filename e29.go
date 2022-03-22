328b45344afee0d717b5c9c43617bd150557ac49
Tom Nguyen <104503-tunggnu@users.noreply.gitlab.com>
2022-03-22 12:09:46 +0700

    Example 29

---
+++ e29.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func e29() {
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good morning!")
+	case t.Hour() < 17:
+		fmt.Println("Good afternoon.")
+	default:
+		fmt.Println("Good evening.")
+	}
+}
--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 package main
 
 func main() {
-	e28()
+	e29()
 }
